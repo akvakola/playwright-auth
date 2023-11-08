63647063cc34fb7f1008b7a6424bbf8300baa97e
ndvik <viktor.novacki@endava.com>
2023-11-08 12:35:28 +0100

    Added gh-a workflow

--- tests/Login.spec.ts
+++ tests/Login.spec.ts
@@ -4,8 +4,15 @@ import "dotenv/config";
 test("Login", async ({ page }) => {
   await page.goto("https://www.demoblaze.com/index.html");
   await page.locator("#login2").click();
-  await page.locator("#loginusername").fill(process.env.USERNAME as string);
-  await page.locator("#loginpassword").fill(process.env.PASSWORD as string);
+  if (
+    process.env.USERNAME !== "undefined" &&
+    process.env.PASSWORD !== "undefined"
+  ) {
+    await page.locator("#loginusername").fill(process.env.USERNAME as string);
+    await page.locator("#loginpassword").fill(process.env.PASSWORD as string);
+  } else {
+    throw new Error("Missing env variables");
+  }
   await page.getByRole("button", { name: "Log in" }).click();
 
   await expect(page.getByText("Welcome vnovacki")).toBeVisible();
