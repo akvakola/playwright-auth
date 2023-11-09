4c2ea248af77afc5032f08d75b9eac9e54754bc3
ndvik <viktor.novacki@endava.com>
2023-11-09 10:47:33 +0100

    removed if statement

--- tests/Login.spec.ts
+++ tests/Login.spec.ts
@@ -4,15 +4,10 @@ import "dotenv/config";
 test("Login", async ({ page }) => {
   await page.goto("https://www.demoblaze.com/index.html");
   await page.locator("#login2").click();
-  if (
-    process.env.USERNAME !== "undefined" &&
-    process.env.PASSWORD !== "undefined"
-  ) {
-    await page.locator("#loginusername").fill(process.env.USERNAME as string);
-    await page.locator("#loginpassword").fill(process.env.PASSWORD as string);
-  } else {
-    throw new Error("Missing env variables");
-  }
+
+  await page.locator("#loginusername").fill(process.env.USERNAME as string);
+  await page.locator("#loginpassword").fill(process.env.PASSWORD as string);
+
   await page.getByRole("button", { name: "Log in" }).click();
 
   await expect(page.getByText("Welcome vnovacki")).toBeVisible();
